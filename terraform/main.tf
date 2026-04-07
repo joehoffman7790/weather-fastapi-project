3c7ce6eb8abe6aef0db2747497413bab33e170a2
jhoffman <joehoffman7790@gmail.com>
2026-04-06 21:15:13 -0400

    refactor(terraform): replace inline tag blocks with reusable local.common_tags

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,28 +1,9 @@
-terraform {
-  required_version = ">= 1.0"
-
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-
 # Resource Group
 resource "azurerm_resource_group" "main" {
   name     = var.resource_group_name
   location = var.location
 
-  tags = {
-    Environment = var.environment
-    Project     = "weather-api"
-    ManagedBy   = "Terraform"
-  }
+  tags = local.common_tags
 }
 
 # Container Registry
@@ -33,9 +14,7 @@ resource "azurerm_container_registry" "acr" {
   sku                 = "Basic"
   admin_enabled       = true
 
-  tags = {
-    Environment = var.environment
-  }
+  tags = local.common_tags
 }
 
 # App Service Plan
@@ -46,9 +25,7 @@ resource "azurerm_service_plan" "plan" {
   os_type             = "Linux"
   sku_name            = var.app_service_sku
 
-  tags = {
-    Environment = var.environment
-  }
+  tags = local.common_tags
 }
 
 # Web App
@@ -62,22 +39,20 @@ resource "azurerm_linux_web_app" "app" {
     always_on = var.app_service_sku == "F1" ? false : true
 
     application_stack {
-  docker_image_name        = "weather-api:latest"  # Remove the server prefix
-  docker_registry_url      = "https://${azurerm_container_registry.acr.login_server}"
-  docker_registry_username = azurerm_container_registry.acr.admin_username
-  docker_registry_password = azurerm_container_registry.acr.admin_password
-}
+      docker_image_name        = "weather-api:latest"
+      docker_registry_url      = "https://${azurerm_container_registry.acr.login_server}"
+      docker_registry_username = azurerm_container_registry.acr.admin_username
+      docker_registry_password = azurerm_container_registry.acr.admin_password
+    }
   }
 
   app_settings = {
-    "WEBSITES_PORT"                    = "8000"
-    "OPENWEATHER_API_KEY"              = var.openweather_api_key
-    "DOCKER_REGISTRY_SERVER_URL"       = "https://${azurerm_container_registry.acr.login_server}"
-    "DOCKER_REGISTRY_SERVER_USERNAME"  = azurerm_container_registry.acr.admin_username
-    "DOCKER_REGISTRY_SERVER_PASSWORD"  = azurerm_container_registry.acr.admin_password
+    "WEBSITES_PORT"                   = "8000"
+    "OPENWEATHER_API_KEY"             = var.openweather_api_key
+    "DOCKER_REGISTRY_SERVER_URL"      = "https://${azurerm_container_registry.acr.login_server}"
+    "DOCKER_REGISTRY_SERVER_USERNAME" = azurerm_container_registry.acr.admin_username
+    "DOCKER_REGISTRY_SERVER_PASSWORD" = azurerm_container_registry.acr.admin_password
   }
 
-  tags = {
-    Environment = var.environment
-  }
+  tags = local.common_tags
 }
